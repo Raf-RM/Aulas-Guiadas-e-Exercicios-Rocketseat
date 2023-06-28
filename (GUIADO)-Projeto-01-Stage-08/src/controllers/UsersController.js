9fce04ff92cb0be961d115a560fbf0afa40ca5d0
Raf-RM <rafael.r.mateus@gmail.com>
2023-06-28 10:18:32 -0300

    add função de update  de usuário

--- (GUIADO)-Projeto-01-Stage-08/src/controllers/UsersController.js
+++ (GUIADO)-Projeto-01-Stage-08/src/controllers/UsersController.js
@@ -28,6 +28,44 @@ class UsersController {
     return response.status(201).json();
 
     }
+
+  async update(request, response) {
+    const { name, email } = request.body; // pegando as informações, através do corpo da requisição, que estão sendo modificadas
+    const { id } = request.params; // pegando a id do usuário através do parâmetro passado no endereço da requisição 
+
+    //criando conexão com o banco de dados
+    const database = await sqliteConnection();
+
+    //buscando pelo usuário
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    //Se o usuário não existir ele lança um erro
+    if (!user) {
+      throw AppError("Usuário não encontrado.");
+    }
+
+    //Verificando se o novo email não possui cadastro
+    const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw AppError("Email já em uso.")
+    }
+
+    // Se passar por todas as verificações
+    user.name = name;
+    user.email = email;
+
+    //executando o update do nosso database (utilizando interpolação) 
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+      );
+
+      return response.status(200).json();
+  }
 }
 
 module.exports = UsersController;
--- (GUIADO)-Projeto-01-Stage-08/src/routes/users.routes.js
+++ (GUIADO)-Projeto-01-Stage-08/src/routes/users.routes.js
@@ -9,6 +9,7 @@ const usersRoutes = Router();
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 // exportando o userRoutes para ser utilizado fora deste arquivo
 module.exports = usersRoutes;
