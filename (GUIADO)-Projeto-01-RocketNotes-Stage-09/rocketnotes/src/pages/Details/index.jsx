fe520de2640f811fa8b7c0cf2694f7ccd65b9c54
Raf-RM <rafael.r.mateus@gmail.com>
2023-09-25 08:04:07 -0300

    add CSS-in-JS, alteracao arquivo styles

--- (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Details/index.jsx
+++ (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
+import { Container } from "./styles";
+
 export function Details() {
   
   return(
-    <>
+    <Container>
       <h1>Hello World!</h1>
       <span>Rafael Rodrigues</span>
-    </>
+    </Container>
   )
 }
 
