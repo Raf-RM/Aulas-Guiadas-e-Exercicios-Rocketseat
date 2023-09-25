eb5e318c51fafd915034c518b882c631683e3056
Raf-RM <rafael.r.mateus@gmail.com>
2023-09-25 10:22:49 -0300

    Componentizacao add componente button

--- (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Details/index.jsx
+++ (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,15 @@
 import { Container } from "./styles";
 
+import { Button } from "../../components/Button";
+
 export function Details() {
   
   return(
     <Container>
       <h1>Hello World!</h1>
       <span>Rafael Rodrigues</span>
+
+      <Button/>
     </Container>
   )
 }
