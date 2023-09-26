054d961d617a8e565d71e7c43a775cc117d84c1e
Raf-RM <rafael.r.mateus@gmail.com>
2023-09-26 18:04:14 -0300

    add componente header

--- (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Details/index.jsx
+++ (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Details/index.jsx
@@ -1,17 +1,14 @@
 import { Container } from "./styles";
 
+import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 
 export function Details() {
   
   return(
     <Container>
-      <h1>Hello World!</h1>
-      <span>Rafael Rodrigues</span>
-
-      <Button label="Entrar" loading />
+      <Header/>
       <Button label="Voltar"/>
-      <Button label="Salvar" loading />
     </Container>
   )
 }
