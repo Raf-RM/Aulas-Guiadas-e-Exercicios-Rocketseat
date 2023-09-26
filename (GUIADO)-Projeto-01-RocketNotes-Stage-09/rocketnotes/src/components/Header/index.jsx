b09ec754ee3ae21c0b5145e422cac909716b6799
Raf-RM <rafael.r.mateus@gmail.com>
2023-09-26 18:41:35 -0300

    add imagem de usuario

--- (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/components/Header/index.jsx
+++ (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,22 @@
-import { Container } from "./styles"
+import { Container, Profile } from "./styles"
 
 export function Header(){
   return (
     <Container>
-      
+      <Profile>
+
+        <img
+          src="https://github.com/Raf-RM.png"
+          alt="Foto github usuário Raf-RM"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Rafael Rodrigues</strong>
+        </div>
+
+      </Profile>
     </Container>
   );
 }
+
--- (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/components/Header/styles.js
+++ (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/components/Header/styles.js
@@ -15,3 +15,34 @@ export const Container = styled.header `
   padding: 0 80px;
 
 `;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    //Garante que vai estilizar somente a imagem que tem dentro do elemento Profile
+
+    width: 56px;
+    height:56px;
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      font-size: 14px;
+      color: ${({ theme })=>theme.COLORS.GRAY_100};
+    }
+
+    strong {
+      font-size: 18px;
+      color: ${({ theme })=>theme.COLORS.WHITE};
+    }
+  }
+
+`;
