3abf66eb3a7ed82583192b914edc1c936dd93faa
Raf-RM <rafael.r.mateus@gmail.com>
2023-10-05 17:01:45 -0300

    add img page SignIn

--- (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/SignIn/index.jsx
+++ (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,9 +1,38 @@
-import { Container } from "./styles";
+import { Container, Form, Background } from "./styles";
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import {FiMail, FiLock} from 'react-icons/fi';
 
 export function SignIn() {
   return (
     <Container>
-      
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação parra salvar e gerenciar seus links úteis.</p>
+        <h2>Faça seu login</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button label="Entrar" />
+
+        <a href="#">
+          Criar conta
+        </a>
+      </Form>
+
+      <Background/>
+
     </Container>
   )
 }
