e7efd567cfd8e13a053fbf62dd230ae0a566418e
Raf-RM <rafael.r.mateus@gmail.com>
2023-10-06 12:22:22 -0300

    add e config foto usuario

--- (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Profile/index.jsx
+++ (GUIADO)-Projeto-01-RocketNotes-Stage-09/rocketnotes/src/pages/Profile/index.jsx
@@ -1,19 +1,35 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { Container, Form } from './styles'
+import { Container, Form, Avatar } from './styles'
 
 export function Profile(){
   return (
     <Container>
       <header>
         <a href="/">
-        <FiArrowLeft/>
+          <FiArrowLeft/>
         </a>
       </header>
 
       <Form>
+
+        <Avatar>
+          <img 
+            src="https://github.com/raf-rm.png" 
+            alt="Foto do usuário" 
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera/>
+
+            <input 
+              id="avatar" 
+              type="file" 
+            />
+          </label>
+        </Avatar>
 
         <Input
           placeholder="Nome"
