ba0499d220ade03206058f2e8a77ca0990c40e23
Raf-RM <rafael.r.mateus@gmail.com>
2023-10-28 15:45:13 -0300

    Inicio da funcionalidade de carregar img

--- (GUIADO)-Projeto-01-Stage-10/API/src/routes/users.routes.js
+++ (GUIADO)-Projeto-01-Stage-10/API/src/routes/users.routes.js
@@ -1,4 +1,6 @@
 const{ Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 // Importando Controller
 const UsersController = require("../controllers/UsersController");
@@ -8,11 +10,17 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
+const upload = multer(uploadConfig.MULTER);
+
 // Instanciando a classe UsersController
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response)=>{
+  console.log(request.file.filename);
+  response.json();
+});
 
 // exportando o userRoutes para ser utilizado fora deste arquivo
 module.exports = usersRoutes;
