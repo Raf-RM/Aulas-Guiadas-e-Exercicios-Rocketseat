82d901d72631569ceb9b1871f83376bd9427a412
Raf-RM <rafael.r.mateus@gmail.com>
2023-11-13 10:34:09 -0300

    add mostrar img e nome usuario no comp Header

--- (GUIADO)-Projeto-01-Stage-10/frontend/src/components/Header/index.jsx
+++ (GUIADO)-Projeto-01-Stage-10/frontend/src/components/Header/index.jsx
@@ -1,23 +1,28 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { useAuth } from '../../hooks/auth';
+import { api } from '../../services/api';
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 
 import { Container, Profile, Logout } from "./styles"
 
 export function Header(){
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
   return (
     <Container>
       <Profile to="/profile" >
 
         <img
-          src="https://github.com/Raf-RM.png"
+          src={avatarURL}
           alt="Foto github usuário Raf-RM"
         />
 
         <div>
           <span>Bem-vindo</span>
-          <strong>Rafael Rodrigues</strong>
+          <strong>{user.name}</strong>
         </div>
 
       </Profile>
